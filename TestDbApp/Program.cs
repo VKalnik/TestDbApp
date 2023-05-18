de9f7f9423d8a3d73fefde075fa376b37722ed80
VKalnik <v.kalnik@mail.ru>
2023-05-18 17:12:04 +0200

    Заготовка каркаса консольного приложения

--- TestDbApp/Program.cs
+++ TestDbApp/Program.cs
@@ -4,7 +4,74 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Отсутствуют аргументы, запуск не возможен!");
+                Console.ReadKey();
+                return;
+            }
+
+            try
+            {
+                int intArg;
+                if (!int.TryParse(args[0], out intArg))
+                {
+                    Console.WriteLine("Первый аргумент не корректный, запуск не возможен!");
+                    Console.ReadKey();
+                    return;
+                }
+
+                switch (intArg)
+                {
+                    case 1:
+                        Console.WriteLine(intArg);
+                        Console.ReadKey();
+                        break;
+
+                    case 2:
+
+                        if (args.Length != 6)
+                        {
+                            Console.WriteLine("Не верное количество или формат введённых аргументов, запуск не возможен!\n"
+                                + "Изпользуйте формат: TestDbApp 2 Фамилия Имя Отчество ДатаРождения Пол");
+                            Console.ReadKey();
+                        }
+                        else
+                        {
+                            foreach (var el in args)
+                                Console.Write(el + " ");
+                            Console.ReadKey();
+                        }
+
+                        break;
+
+                    case 3:
+                        Console.WriteLine(intArg);
+                        Console.ReadKey();
+                        break;
+
+                    case 4:
+                        Console.WriteLine(intArg);
+                        Console.ReadKey();
+                        break;
+
+                    case 5:
+                        Console.WriteLine(intArg);
+                        Console.ReadKey();
+                        break;
+
+                    default:
+                        Console.WriteLine("Введено не верное значение первого аргумента, запуск не возможен!");
+                        Console.ReadKey();
+                        break;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                throw;
+            }
         }
     }
 }
