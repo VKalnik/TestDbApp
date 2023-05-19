a9efef5d16a85bf308cf2e7a3bb0f09b13473132
VKalnik <v.kalnik@mail.ru>
2023-05-19 02:58:40 +0200

    Изменен порядок свойств в соответствии с таблицей БД

--- TestDb.Models/Person.cs
+++ TestDb.Models/Person.cs
@@ -3,9 +3,9 @@
     public class Person
     {
         public int Id { get; set; }
+        public string LastName { get; set; }
         public string FirstName { get; set; }
         public string Patronymic { get; set; }
-        public string LastName { get; set; }
         public DateTime BirthDate { get; set; }
         public string Gender { get; set; }
     }
