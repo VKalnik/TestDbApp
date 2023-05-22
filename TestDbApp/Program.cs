54ff46989d912bd9aeb8085b9fc009a2aeacde74
VKalnik <v.kalnik@mail.ru>
2023-05-22 16:43:29 +0200

    Рефакторинг, финальные правки

--- TestDbApp/Program.cs
+++ TestDbApp/Program.cs
@@ -4,9 +4,6 @@ using TestData;
 using TestDb.Interfaces;
 using TestDb.Models;
 using TestDB.MsSql;
-using TestDB.MsSql.Services;
-using System.Linq.Expressions;
-using Microsoft.VisualBasic;
 
 
 namespace TestDbApp
@@ -56,6 +53,7 @@ namespace TestDbApp
                 {
                     case 1:
                         dbService.AddTable(conectionString);
+                        Console.WriteLine("Таблица [Persons] создана в БД");
                         break;
 
                     case 2:
@@ -63,7 +61,6 @@ namespace TestDbApp
                         {
                             Console.WriteLine("Не верное количество или формат введённых аргументов, запуск не возможен!\n"
                                 + "Изпользуйте формат: TestDbApp 2 Фамилия Имя Отчество ДатаРождения Пол");
-                            Console.ReadKey();
                         }
                         else
                         {
@@ -78,35 +75,42 @@ namespace TestDbApp
 
                             dbService.AddRow(conectionString, person);
                         }
+
+                        Console.WriteLine("Строка добавлена в БД.");
+
                         break;
 
                     case 3:
-                        string sqlCommandText = "";
+                        string sqlCommandText = "SELECT id, last_name,  first_name,  patronymic,  birthdate,  gender FROM (SELECT *, ROW_NUMBER() OVER (PARTITION BY last_name,  first_name,  patronymic,  birthdate ORDER BY last_name) as row_num FROM Persons) AS subquery WHERE row_num = 1 ORDER BY last_name;";
                         
-                        var peoples = dbService.Select(conectionString);
-                        Console.Clear();
-                        foreach (Person p in peoples)
-                            Console.WriteLine($"ФИО: {p.LastName} {p.FirstName} {p.Patronymic}, "
-                                            + $"Дата рождения: {p.BirthDate:dd.MM.yyyy}, "
-                                            + $"Пол: {p.Gender}, "
-                                            + $"Полных лет: {p.GetAge()}");
+                        var peoples = dbService.Select(conectionString, sqlCommandText);
 
- 
+                        foreach (Person p in peoples.persons)
+                            Console.WriteLine(p + $"Полных лет: {p.GetAge()}");
                         break;
 
                     case 4:
-                        var persons = testDataService.GetRandomPersons(1000);
+
+                        int amount = 1000000;
+                        var persons = testDataService.GetRandomPersons(amount);
                         dbService.AddRows(conectionString, persons);
+
+                        Console.WriteLine($"В таблицу [Persons] в БД добавлено {amount} строк");
+
                         break;
 
                     case 5:
-                        Console.WriteLine(intArg);
-                        Console.ReadKey();
+                        string sqlCommandText1 = "SELECT * FROM Persons WHERE gender = 'M' AND last_name LIKE 'F%';";
+
+                        foreach (Person p in dbService.Select(conectionString, sqlCommandText1).persons)
+                            Console.WriteLine(p);
+
+                        Console.WriteLine(dbService.Select(conectionString, sqlCommandText1).msg);
+
                         break;
 
                     default:
                         Console.WriteLine("Введено не верное значение первого аргумента, запуск не возможен!");
-                        Console.ReadKey();
                         break;
                 }
             }
